54c320f0d13b7040d69087698c6e08ac79729400
KenLe21 <kien2108dn@gmail.com>
2024-01-08 17:02:06 +0700

    update zombiefather

---
+++ src/Zombie/ZombieFather.java
@@ -0,0 +1,38 @@
+package Zombie;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public interface ZombieFather {
+    void advance();
+
+
+    void takeDamage(int damage);
+
+
+    void setColor(Color color);
+
+
+    void changeColor(Color color);
+
+
+    void slowDown();
+
+
+    void applyDamageEffects();
+
+
+    boolean isAlive();
+
+
+    void die();
+
+    void playDeathAnimation();
+
+
+    void dropItems();
+    void playdDeathSound();
+    void keyPressed(KeyEvent e);
+}
+
+
